1383c7e6d92ded166d0c4815baa4cc3060ff1aaf
MichaelPopov62 <romantikzizni@gmail.com>
2025-12-20 14:36:36 +0200

    Update vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,51 +1,3 @@
-// import imagemin from "imagemin";
-// import imageminWebp from "imagemin-webp";
-// import path from "path";
-// import { defineConfig } from "vite";
-// import glob from "fast-glob";
-// import { fileURLToPath } from "url";
-// import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
-
-// export default defineConfig({
-//   plugins: [
-//     ViteImageOptimizer({
-//       png: {
-//         quality: 86,
-//       },
-//       jpeg: {
-//         quality: 86,
-//       },
-//       jpg: {
-//         quality: 86,
-//       },
-//     }),
-//     {
-//       ...imagemin(["./src/img/**/*.{jpg,png,jpeg}"], {
-//         destination: "./src/img/webp/",
-//         plugins: [imageminWebp({ quality: 86 })],
-//       }),
-//       apply: "serve",
-//     },
-//   ],
-//   build: {
-//     minify: false, // disable minification
-//     rollupOptions: {
-//       input: Object.fromEntries(
-//         glob
-//           .sync(["./*.html", "./pages/**/*.html"])
-//           .map((file) => [
-//             path.relative(__dirname, file.slice(0, file.length - path.extname(file).length)),
-//             fileURLToPath(new URL(file, import.meta.url)),
-//           ])
-//       ),
-//       // output unminified CSS file
-//       output: {
-//         assetFileNames: "assets/[name].[ext]",
-//       },
-//     },
-//   },
-// });
-
 import path from "path";
 import { defineConfig } from "vite";
 import glob from "fast-glob";
@@ -53,6 +5,7 @@ import { fileURLToPath } from "url";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
+  base: '/vite-sass-study/', // <-- тут назва твого репозиторію на GitHub
   plugins: [
     ViteImageOptimizer({
       png: { quality: 86 },
@@ -62,7 +15,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // для абсолютних імпортів SCSS/JS
+      '@': path.resolve(__dirname, 'src'), // абсолютні імпорти JS/SCSS
     },
   },
   build: {
@@ -82,4 +35,3 @@ export default defineConfig({
     },
   },
 });
-
